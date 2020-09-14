6b89af24910069c2a75c356667f03703c9420f05
Bob Weigel <Robert.S.Weigel@gmail.com>
2020-09-14 08:34:04 -0400

    HW1 grade and comments

--- HW1_1.m
+++ HW1_1.m
@@ -1,6 +1,7 @@
 clc;clear all;
-k = 8.987E9; 
-p = [1,0; -1,0]; % location of points
+k = 8.987E9;
+d = 1;
+p = [d,0; -d,0]; % location of points
 Q = [1; -1]; % 1 and -1 Coulomb charge
 t=0.05;
 [X,Y] = meshgrid(-2:t:2);
@@ -10,12 +11,21 @@ for ii = 1:numel(Q)
     V = V + k * Q(ii)./ (((p(ii,1)-X).^2 + (p(ii,2)-Y).^2).^(1/2)); %calculate electric potential at points
 end
 
-contourf(X,Y,V)
+colormap(parula(15)) % It does not makes sense for colorbar to have so
+% many colors when plot essentially shows 3. Should also plot lot(V), but
+% negative values would need special treatment.
+Vo = (k*Q(1)/d^2); % I'd use dimensionless variables
+contourf(X,Y,V/Vo)
 hColorbar = colorbar;
-ylabel(hColorbar,'Electric potential (V)')
+ylabel(hColorbar,'Electric potential V/V_o')
 title('Field Lines of Point Charges 1C and -1C')
 
 [Ex,Ey] = gradient(V);
 C = all(isfinite(Ex) & isfinite(Ey));
+% You found it!
 field = streamslice(X(:,C),Y(:,C),-Ex(:,C),-Ey(:,C));
+xlabel('x/d');
+ylabel('y/d');
 set(field,'Color','k');
+axis square
+print -dpdf -bestfit HW1_1.pdf
--- HW1_2.m
+++ HW1_2.m
@@ -6,29 +6,35 @@ clc;clear all;
 %http://dev.physicslab.org/Document.aspx?doctype=3&filename=Electrostatics_ContinuousChargedRod.xml
 
 fprintf("Running... this takes about a minute to complete\n");
-n=250; %number of max iterations for loop
+n=50; %number of max iterations for loop
 j=2; %number of charges, starting with 2 (smallest number of charges you can have to approximate a line charge)
 e=1; %error value
 tab = zeros(j-1,2); %for table to be printed
-while ((e > 0.01) & j<n) %run loop until error reaches < or = 0.01, or for n iterations, to prevent endless loop
 k = 8.987E9; 
-p = ones(j,2); %for location of charges
-for i=1:1:j
-   p(i,:) = [2*((i-1)/(j-1))-1,0]; %array of point locations
-end
-Q = ones(j,1); %array of point charges of 1 Coulomb charge
-j=j+1;
-t=0.01;
-[X,Y] = meshgrid(-2:t:2);
+while ((e > 0.01) && j<n) %run loop until error reaches < or = 0.01, or for n iterations, to prevent endless loop
+    p = ones(j,2); %for location of charges
+    for i=1:1:j
+       p(i,:) = [2*((i-1)/(j-1))-1,0]; %array of point locations
+    end
+    Q = ones(j,1); %array of point charges of 1 Coulomb charge
+    j=j+1;
+    t=0.01;
+    [X,Y] = meshgrid(-2:t:2);
 
-V = zeros(size(X)); 
-for ii = 1:numel(Q) 
-    V = V + k * Q(ii) *(p(ii,1)-X)./ (((p(ii,1)-X).^2 + (p(ii,2)-Y).^2).^(1/2)); %calculate electric potential at points
-end
-v=-gradient(V); %calculate electric field
-c=(v((1/t)+1, ((4/t)/2)+1)); %electric field at point [0,L], with L=1
-e=(((1.2710E10)-c)/(1.27106E10)); %error
-tab(j-2,:)=[j,e]; %fill table
+    V = zeros(size(X)); 
+    for ii = 1:numel(Q) 
+        V = V + k * Q(ii) *(p(ii,1)-X)./ (((p(ii,1)-X).^2 + (p(ii,2)-Y).^2).^(1/2)); %calculate electric potential at points
+    end
+    % This introduces another error - we want to know the error due
+    % to discretization and this introduces an error due to the
+    % approximated method used to compute the electric field. Should
+    % use the exact formula for E for point charges instead. Did you 
+    % check your first few calculations by hand? Even given this
+    % approxmation, your N seems too high.
+    v=-gradient(V); %calculate electric field
+    c=(v((1/t)+1, ((4/t)/2)+1)); %electric field at point [0,L], with L=1
+    e=(((1.2710E10)-c)/(1.27106E10)); %error
+    tab(j-2,:)=[j,e]; %fill table
 end
 fprintf("Number of point charges: " + string(j)+'\n');
 fprintf("Error: " + string(e)+'\n');
